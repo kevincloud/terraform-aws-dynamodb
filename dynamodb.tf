f99848d4ddc578683edf7b6fe8b106bf29a9ea61
Kevin Cochran <kcochran@hashicorp.com>
2020-05-27 18:56:24 -0400

    initial commit

---
+++ dynamodb.tf
@@ -0,0 +1,18 @@
+resource "aws_dynamodb_table" "item-data-table" {
+    name = "standard-ddb-${var.identifier}"
+    billing_mode = "PROVISIONED"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = var.key_setup.hashkey.keyname
+    range_key = var.key_setup.rangekey.keyname
+    
+    attribute {
+        name = var.key_setup.hashkey.keyname
+        type = var.key_setup.hashkey.keydata
+    }
+    
+    attribute {
+        name = var.key_setup.rangekey.keyname
+        type = var.key_setup.rangekey.keydata
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "identifier" { }
+variable "key_setup" {
+    type = map(object({
+        keyname = string
+        keydata = string
+    }))
+    default = {
+        hashkey = {
+            keyname = "MyHash"
+            keydata = "S"
+        }
+        rangekey = {
+            keyname = "MyRange"
+            keydata = "S"
+        }
+    }
+}
